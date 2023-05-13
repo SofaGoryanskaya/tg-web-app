38a6bd1873164d29884930450671fb3d3963d19b
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-13 13:49:27 +0700

    front check fench

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,51 +1,19 @@
-// import React, {useState} from 'react';
-// import './ProductItem.css';
-// import Button from "../Button/Button";
-//
-//  const ProductItem = ({product, className, onAdd, removeProduct, counter, flag}) => {
-//
-//     const onAddHandler = () => {
-//         onAdd(product);
-//
-//     }
-//     const handleDecrement = (flag) => {
-//         removeProduct(product);
-//     }
-//      const counterProduct = () => {
-//          counter(product);
-//      }
-//     return (
-//         <div className={'product ' + className}>
-//             <div className={'img'}/>
-//             <div className={'title'}>{product.title}</div>
-//             <div className={'description'}>{product.description}</div>
-//             <div className={'price'}>
-//                 <span>Стоимость: <b>{product.price}</b></span>
-//             </div>
-//             <button className={'count'} disabled> {product.count} </button>
-//             <Button className={'add-btn'} onClick={onAddHandler}>
-//                 Добавить в корзину
-//             </Button>
-//             <Button className={'remove'} onClick={handleDecrement}>
-//                 Удалить
-//             </Button>
-//         </div>
-//     );
-// };
-//
-// export default ProductItem;
-
-
-import React from 'react';
-import Button from "../Button/Button";
+import React, {useState} from 'react';
 import './ProductItem.css';
+import Button from "../Button/Button";
 
-const ProductItem = ({product, className, onAdd}) => {
+ const ProductItem = ({product, className, onAdd, removeProduct, counter, flag}) => {
 
     const onAddHandler = () => {
         onAdd(product);
-    }
 
+    }
+    const handleDecrement = (flag) => {
+        removeProduct(product);
+    }
+     const counterProduct = () => {
+         counter(product);
+     }
     return (
         <div className={'product ' + className}>
             <div className={'img'}/>
@@ -54,8 +22,12 @@ const ProductItem = ({product, className, onAdd}) => {
             <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
+            <button className={'count'} disabled> {product.count} </button>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
+            </Button>
+            <Button className={'remove'} onClick={handleDecrement}>
+                Удалить
             </Button>
         </div>
     );
