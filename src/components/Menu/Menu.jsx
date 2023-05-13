4255aaf72bffe4b440fe2a5257b609b2a4b1f032
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-13 13:44:28 +0700

    front check fench

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,22 +1,124 @@
-import React, {useCallback, useEffect, useState} from 'react';
+// import React, {useCallback, useEffect, useState} from 'react';
+// import './Menu.css';
+// import ProductItem from "../ProductItem/ProductItem";
+// import {useTelegram} from "../../hooks/useTelegram";
+// import Header from "../Header/Header";
+//
+//
+// //функция по нахождению общей стоимости корзины
+// const getTotalPrice = (items = []) => {
+//     return items.reduce((acc, item) => {
+//             return acc += item.price
+//
+//     }, 0)
+// }
+//
+// //список продуктов
+// const products = [
+//     {id: '1', title: 'Латте Таро', price: 1, description: '2 л', count: 0},
+//     {id: '2', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
+//     {id: '3', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
+//     {id: '4', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
+//     {id: '5', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
+// ]
+// const Menu = (callback, deps) => {
+//     const {tg, queryId} = useTelegram();
+//     //для добавления и удаления товаров из корзины
+//     const [addedItems, setAddedItems] = useState([]);
+//
+//     const onAdd= (product) => {
+//         const alreadyAdded = addedItems.find(item => item.id === product.id);
+//         let newItems = [];
+//
+//        //добавление
+//             newItems = [...addedItems, product];
+//             product.count += 1;
+//
+//         setAddedItems(newItems)
+//
+//             tg.MainButton.show();
+//             tg.MainButton.setParams({
+//                 text: `Купить ${getTotalPrice(newItems)}`,
+//                 "color": "#583635",
+//             })
+//         tg.offEvent('mainButtonClicked', tg.sendData('1111'))
+//
+//     }
+//     const removeProduct = (product) => {
+//         const alreadyAdded = addedItems.find(item => item.id === product.id);
+//         let flag = true;
+//         let newItems = [];
+//         if (product.count > 0) {
+//           // newItems= addedItems.splice(product.id);
+//             product.count = 0;
+//             newItems = addedItems.filter(item => item.id !== product.id);
+//         }
+//
+//         setAddedItems(newItems)
+//
+//         tg.MainButton.setParams({
+//             text: `Купить ${getTotalPrice(newItems)}`,
+//             "color": "#583635",
+//         })
+//     }
+//
+//     const onSendData = useCallback(() => {
+//         const data = {
+//             products: addedItems,
+//             totalPrice: getTotalPrice(addedItems),
+//             queryId,
+//         }
+//         fetch('https://79.141.77.109:8080/web-data', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify(data)
+//         })
+//     }, [addedItems])
+//
+//     useEffect(() => {
+//         tg.onEvent('mainButtonClicked', onSendData)
+//         return () => {
+//             tg.offEvent('mainButtonClicked', onSendData)
+//         }
+//     }, [onSendData])
+//
+//
+//     //для кнопки Купить, queryId - отправляем в бэк и обмениваемя информацией
+//     //отпрвляем запрос
+//
+//
+//     return (
+//         <div className={'list'}>
+//             <span className={'headerName'}>
+//                 Университет кофе
+//             </span>
+//
+//            <Header/>
+//             {products.map(item => (
+//                 <ProductItem
+//                     product={item}
+//                     onAdd={onAdd}
+//                     removeProduct = {removeProduct}
+//                     className={'item'}
+//                 />
+//             ))}
+//         </div>
+//
+//
+//     );
+// };
+//
+// export default Menu;
+
+
+import React, {useState} from 'react';
 import './Menu.css';
 import ProductItem from "../ProductItem/ProductItem";
 import {useTelegram} from "../../hooks/useTelegram";
-import Button from "../Button/Button";
-import Main from "../Main/Main";
-import Header from "../Header/Header";
+import {useCallback, useEffect} from "react";
 
-const {getData} = require('../BD/BD');
-
-//функция по нахождению общей стоимости корзины
-const getTotalPrice = (items = []) => {
-    return items.reduce((acc, item) => {
-            return acc += item.price
-
-    }, 0)
-}
-
-//список продуктов
 const products = [
     {id: '1', title: 'Латте Таро', price: 1, description: '2 л', count: 0},
     {id: '2', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
@@ -24,46 +126,16 @@ const products = [
     {id: '4', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
     {id: '5', title: 'Латте Таро', price: 1, description: '350 мл', count: 0},
 ]
-const Menu = (callback, deps) => {
-    const {tg, queryId} = useTelegram();
-    //для добавления и удаления товаров из корзины
+
+const getTotalPrice = (items = []) => {
+    return items.reduce((acc, item) => {
+        return acc += item.price
+    }, 0)
+}
+
+const Menu = () => {
     const [addedItems, setAddedItems] = useState([]);
-
-    const onAdd= (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
-        let newItems = [];
-
-       //добавление
-            newItems = [...addedItems, product];
-            product.count += 1;
-
-        setAddedItems(newItems)
-
-            tg.MainButton.show();
-            tg.MainButton.setParams({
-                text: `Купить ${getTotalPrice(newItems)}`,
-                "color": "#583635",
-            })
-        tg.offEvent('mainButtonClicked', tg.sendData('1111'))
-
-    }
-    const removeProduct = (product) => {
-        const alreadyAdded = addedItems.find(item => item.id === product.id);
-        let flag = true;
-        let newItems = [];
-        if (product.count > 0) {
-          // newItems= addedItems.splice(product.id);
-            product.count = 0;
-            newItems = addedItems.filter(item => item.id !== product.id);
-        }
-
-        setAddedItems(newItems)
-
-        tg.MainButton.setParams({
-            text: `Купить ${getTotalPrice(newItems)}`,
-            "color": "#583635",
-        })
-    }
+    const {tg, queryId} = useTelegram();
 
     const onSendData = useCallback(() => {
         const data = {
@@ -71,7 +143,7 @@ const Menu = (callback, deps) => {
             totalPrice: getTotalPrice(addedItems),
             queryId,
         }
-        fetch('https://79.141.77.109:8080/web-data', {
+        fetch('https://192.168.0.2:8080/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -87,29 +159,38 @@ const Menu = (callback, deps) => {
         }
     }, [onSendData])
 
+    const onAdd = (product) => {
+        const alreadyAdded = addedItems.find(item => item.id === product.id);
+        let newItems = [];
 
-    //для кнопки Купить, queryId - отправляем в бэк и обмениваемя информацией
-    //отпрвляем запрос
+        if(alreadyAdded) {
+            newItems = addedItems.filter(item => item.id !== product.id);
+        } else {
+            newItems = [...addedItems, product];
+        }
 
+        setAddedItems(newItems)
+
+        if(newItems.length === 0) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+            tg.MainButton.setParams({
+                text: `Купить ${getTotalPrice(newItems)}`
+            })
+        }
+    }
 
     return (
         <div className={'list'}>
-            <span className={'headerName'}>
-                Университет кофе
-            </span>
-
-           <Header/>
             {products.map(item => (
                 <ProductItem
                     product={item}
                     onAdd={onAdd}
-                    removeProduct = {removeProduct}
                     className={'item'}
                 />
             ))}
         </div>
-
-
     );
 };
 
--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,19 +1,51 @@
-import React, {useState} from 'react';
-import './ProductItem.css';
-import Button from "../Button/Button";
+// import React, {useState} from 'react';
+// import './ProductItem.css';
+// import Button from "../Button/Button";
+//
+//  const ProductItem = ({product, className, onAdd, removeProduct, counter, flag}) => {
+//
+//     const onAddHandler = () => {
+//         onAdd(product);
+//
+//     }
+//     const handleDecrement = (flag) => {
+//         removeProduct(product);
+//     }
+//      const counterProduct = () => {
+//          counter(product);
+//      }
+//     return (
+//         <div className={'product ' + className}>
+//             <div className={'img'}/>
+//             <div className={'title'}>{product.title}</div>
+//             <div className={'description'}>{product.description}</div>
+//             <div className={'price'}>
+//                 <span>Стоимость: <b>{product.price}</b></span>
+//             </div>
+//             <button className={'count'} disabled> {product.count} </button>
+//             <Button className={'add-btn'} onClick={onAddHandler}>
+//                 Добавить в корзину
+//             </Button>
+//             <Button className={'remove'} onClick={handleDecrement}>
+//                 Удалить
+//             </Button>
+//         </div>
+//     );
+// };
+//
+// export default ProductItem;
 
- const ProductItem = ({product, className, onAdd, removeProduct, counter, flag}) => {
+
+import React from 'react';
+import Button from "../Button/Button";
+import './ProductItem.css';
+
+const ProductItem = ({product, className, onAdd}) => {
 
     const onAddHandler = () => {
         onAdd(product);
+    }
 
-    }
-    const handleDecrement = (flag) => {
-        removeProduct(product);
-    }
-     const counterProduct = () => {
-         counter(product);
-     }
     return (
         <div className={'product ' + className}>
             <div className={'img'}/>
@@ -22,12 +54,8 @@ import Button from "../Button/Button";
             <div className={'price'}>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
-            <button className={'count'} disabled> {product.count} </button>
             <Button className={'add-btn'} onClick={onAddHandler}>
                 Добавить в корзину
-            </Button>
-            <Button className={'remove'} onClick={handleDecrement}>
-                Удалить
             </Button>
         </div>
     );
