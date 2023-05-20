98749df33fc3a97a2737746c433375d3b1bcdf89
SofyaGoryanskaya <sofyagoryanskaya014@gmail.com>
2023-05-20 16:04:10 +0700

    коммит депло

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    //для навигации в Single Page Appl
+    //для навигации в Single Page App
 
   <React.StrictMode>
       <BrowserRouter>
